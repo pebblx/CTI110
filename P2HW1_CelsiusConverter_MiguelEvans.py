9d2a11d8fabdadf84b08bfea4020be22bfa300e4
pebblx <43585268+pebblx@users.noreply.github.com>
2018-10-02 14:31:35 -0400

    Add files via upload

---
+++ P2HW1_CelsiusConverter_MiguelEvans.py
@@ -0,0 +1,10 @@
+# A Simple Celsius Fahrenheit Converter
+# 09-25-18
+# CTI-110 P2HW1 - Celsius Fahrenheit Converter 
+# Miguel Evans
+
+print('This is the Celsius to Fahrenheit Converter') # State what program does
+C = int(input('Enter your Celsius temperature:')) # Input Celsius temp.
+F = (9/5)* C + 32 # Write formula
+print('This is your temperature in Fahrenheit:', F) #Display conversion 
+
